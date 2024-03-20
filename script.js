9777891f1222f6a37d1d1e9204525979ab2be4f1
Prateek Kumar <kumarprateek1833@gmail.com>
2024-03-20 15:45:27 +0530

    Hasburger added

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 document.querySelector('.cross').style.display='none';
+document.querySelector('.hasburger').addEventListener("click",()=>{
+    document.querySelector(".sidebar").classList.toggle("sidebarGo");
+
+})
