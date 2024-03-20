315fee66a32749e76eaec1932462cf25e7603206
Prateek Kumar <kumarprateek1833@gmail.com>
2024-03-20 18:44:24 +0530

    Responsive

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-document.querySelector('.cross').style.display='none';
 document.querySelector('.hasburger').addEventListener("click",()=>{
     document.querySelector(".sidebar").classList.toggle("sidebarGo");
 
