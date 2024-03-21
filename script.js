63d2943f31fa11e2f3520ba6d3f224f6bd2b2ffa
Prateek Kumar <kumarprateek1833@gmail.com>
2024-03-21 18:34:58 +0530

    Buttons

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 document.querySelector('.hasburger').addEventListener("click",()=>{
     document.querySelector(".sidebar").classList.toggle("sidebarGo");
-
 })
+document.querySelector('.pages').addEventListener("click",()=>{
+    document.querySelector(".sidebar").classList.toggle("sidebarGo");
+})
+var element = document.getElementById("menu");
+
+element.addEventListener("click",()=>{
+    document.querySelector(".sidebar").classList.remove("sidebarGo");
+})
+
